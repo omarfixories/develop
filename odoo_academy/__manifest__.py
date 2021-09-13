073601b49a916b4357844f644c8574698a9099ab
Omar Hernandez <omar.hernandez@fixories.com>
2021-09-13 00:25:57 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name'         : 'Odoo Academy',
+    'summary'      : """Academy app to manage Training""",
+    'description'  : """
+        Academy app to manage Training
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    'Author'       : 'Odoo',
+    'website'      : 'https://www.fixories.com',
+    'category'     : 'Training',
+    'version'      : '0.1',
+    'depends'      : ['base'],
+    'data'         : [],
+    'demo'         : [],
+}
