0a2104fbd45a63dce98781e2715f09c5f57ee7ca
Omar Hernandez <omar.hernandez@fixories.com>
2021-09-13 01:48:11 +0000

    [ADD] modules biblioteca,mision,voluntarios

---
+++ fixo_biblioteca_admin/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name'         : 'Misión Espacial',
+    'summary'      : """Planifique y ayude a Odoo a llegar a la Luna""",
+    'description'  : """
+        Planifique y ayude a Odoo a llegar a la Luna
+        -Naves espaciales
+        -Tripulación
+    """,
+    'Author'       : 'Fixories',
+    'website'      : 'https://www.fixories.com',
+    'category'     : 'Logistica',
+    'version'      : '0.1',
+    'depends'      : ['base'],
+    'data'         : [],
+    'demo'         : [],
+}
---
+++ fixo_voluntarios_cooperativos/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name'         : 'Voluntarios Cooperativos',
+    'summary'      : """Organizar voluntarios y tienda""",
+    'description'  : """
+        Organizar voluntarios y tienda
+        -Voluntarios
+        -Tienda
+        -Inventarios
+        -punto de venta
+    """,
+    'Author'       : 'Fixories',
+    'website'      : 'https://www.fixories.com',
+    'category'     : 'Logistica',
+    'version'      : '0.1',
+    'depends'      : ['base'],
+    'data'         : [],
+    'demo'         : [],
+}
