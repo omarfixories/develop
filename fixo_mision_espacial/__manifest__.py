d9e5424794faa411b58e63d721fb22c1c99259e3
Omar Hernandez <omar.hernandez@fixories.com>
2021-09-13 01:48:11 +0000

    [ADD] modules biblioteca,mision,voluntarios

---
+++ fixo_mision_espacial/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name'         : 'Administración de biblioteca',
+    'summary'      : """Administrar libros y clientes""",
+    'description'  : """
+        Administrar libros y clientes
+        -Administración de libros
+        -Administración de clientes
+        -Administración de alquileres
+    """,
+    'Author'       : 'Fixories',
+    'website'      : 'https://www.fixories.com',
+    'category'     : 'Logistica',
+    'version'      : '0.1',
+    'depends'      : ['base'],
+    'data'         : [],
+    'demo'         : [],
+}
